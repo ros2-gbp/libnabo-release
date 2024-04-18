160592c7ea514f53d953d86d718910a908cbfd21
Marco A. Gutierrez <marcogg@marcogg.com>
2024-04-18 22:52:05 +0000

    Rebase from 'upstream'

--- build_system/utilities/norlab-build-system/utilities/norlab-shell-script-tools/tests/bats_testing_tools/bats_helper_functions.bash
+++ build_system/utilities/norlab-build-system/utilities/norlab-shell-script-tools/tests/bats_testing_tools/bats_helper_functions.bash
@@ -47,3 +47,46 @@ function fake_IS_TEAMCITY_RUN() {
         TEAMCITY_VERSION=fake
     fi
 }
+
+# ====bats-file utility============================================================================
+
+# Temporary workaround for missing implementation of "assert_file_not_contains"
+# ToDo: remove when bats-file PR #61 is merge
+#   - PR #61 at https://github.com/bats-core/bats-file/pull/61
+#
+# Credit https://github.com/bats-core/bats-file/pull/61
+#
+# Fail and display path of the file (or directory) if it does contain a string.
+# This function is the logical complement of `assert_file_contains'.
+#
+# Globals:
+#   BATSLIB_FILE_PATH_REM
+#   BATSLIB_FILE_PATH_ADD
+# Arguments:
+#   $1 - path
+#   $2 - regex
+# Returns:
+#   0 - file does not contain regex
+#   1 - otherwise
+# Outputs:
+#   STDERR - details, on failure
+assert_file_not_contains() {
+  local -r file="$1"
+  local -r regex="$2"
+
+  if [[ ! -f "$file" ]]; then
+    local -r rem="${BATSLIB_FILE_PATH_REM-}"
+    local -r add="${BATSLIB_FILE_PATH_ADD-}"
+    batslib_print_kv_single 4 'path' "${file/$rem/$add}" 'regex' "$regex" \
+      | batslib_decorate 'file does not exist' \
+      | fail
+
+  elif grep -q "$regex" "$file"; then
+    local -r rem="${BATSLIB_FILE_PATH_REM-}"
+    local -r add="${BATSLIB_FILE_PATH_ADD-}"
+    batslib_print_kv_single 4 'path' "${file/$rem/$add}" 'regex' "$regex" \
+      | batslib_decorate 'file contains regex' \
+      | fail
+
+  fi
+}
